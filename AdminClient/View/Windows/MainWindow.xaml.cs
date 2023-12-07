e2d6f37fcf7786a79b5ca069051f346dbe797d30
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2023-12-07 13:26:23 +0100

    Admin Client - MVVM structure and base layout

---
+++ AdminClient/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Media;
+
+namespace AdminClient.View.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            Color color = Colors.HotPink;
+            Wpf.Ui.Appearance.Accent.Apply(color);
+
+            InitializeComponent();
+        }
+
+    }
+}
